fa82fd8220f484ef81727826cd26ec9ea1647776
Camilovelag <camilo.vela.galindo@gmail.com>
2022-07-19 15:30:16 -0500

    ESlinter error fixing

--- scripts.js
+++ scripts.js
@@ -1,4 +1,17 @@
-function showMenu() {
+const hamburgerOpen = document.querySelector(".hamburger-btn");
+
+// function hideMenu() {
+//   const div4 = document.querySelector('#links-container');
+//   div4.classList.remove('active-menu');
+
+//   const div5 = document.querySelector('#hamburger-btn');
+//   div5.classList.remove('switch-btn');
+
+//   const div6 = document.querySelector('#header-logo');
+//   div6.classList.remove('active-logo');
+// }
+
+hamburgerOpen.addEventListener("click", () => {
   const div = document.querySelector('#links-container');
   div.classList.toggle('active-menu');
 
@@ -7,15 +20,4 @@ function showMenu() {
 
   const div3 = document.querySelector('#header-logo');
   div3.classList.toggle('active-logo');
-}
-
-function hideMenu() {
-  const div4 = document.querySelector('#links-container');
-  div4.classList.remove('active-menu');
-
-  const div5 = document.querySelector('#hamburger-btn');
-  div5.classList.remove('switch-btn');
-
-  const div6 = document.querySelector('#header-logo');
-  div6.classList.remove('active-logo');
-}
+});
