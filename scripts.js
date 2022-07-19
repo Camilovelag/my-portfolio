34eeca52b78d5af124e1b350fe6c8f4d5a76c8e7
Camilovelag <camilo.vela.galindo@gmail.com>
2022-07-19 15:53:27 -0500

    Fix js function call

--- scripts.js
+++ scripts.js
@@ -1,17 +1,6 @@
-const hamburgerOpen = document.querySelector(".hamburger-btn");
+const hamburgerOpen = document.querySelector('.hamburger-btn');
 
-// function hideMenu() {
-//   const div4 = document.querySelector('#links-container');
-//   div4.classList.remove('active-menu');
-
-//   const div5 = document.querySelector('#hamburger-btn');
-//   div5.classList.remove('switch-btn');
-
-//   const div6 = document.querySelector('#header-logo');
-//   div6.classList.remove('active-logo');
-// }
-
-hamburgerOpen.addEventListener("click", () => {
+hamburgerOpen.addEventListener('click', () => {
   const div = document.querySelector('#links-container');
   div.classList.toggle('active-menu');
 
@@ -20,4 +9,44 @@ hamburgerOpen.addEventListener("click", () => {
 
   const div3 = document.querySelector('#header-logo');
   div3.classList.toggle('active-logo');
+});
+
+
+const menuRecentWorks = document.querySelector('.link-1');
+
+menuRecentWorks.addEventListener('click', () => {
+  const div4 = document.querySelector('#links-container');
+  div4.classList.remove('active-menu');
+
+  const div5 = document.querySelector('#hamburger-btn');
+  div5.classList.remove('switch-btn');
+
+  const div6 = document.querySelector('#header-logo');
+  div6.classList.remove('active-logo');
+});
+
+const menuAboutMe = document.querySelector('.link-2');
+
+menuAboutMe.addEventListener('click', () => {
+  const div4 = document.querySelector('#links-container');
+  div4.classList.remove('active-menu');
+
+  const div5 = document.querySelector('#hamburger-btn');
+  div5.classList.remove('switch-btn');
+
+  const div6 = document.querySelector('#header-logo');
+  div6.classList.remove('active-logo');
+});
+
+const menuContactUs = document.querySelector('.link-3');
+
+menuContactUs.addEventListener('click', () => {
+  const div4 = document.querySelector('#links-container');
+  div4.classList.remove('active-menu');
+
+  const div5 = document.querySelector('#hamburger-btn');
+  div5.classList.remove('switch-btn');
+
+  const div6 = document.querySelector('#header-logo');
+  div6.classList.remove('active-logo');
 });
