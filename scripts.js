e198432fa138ce65169c0f3a850708a6a0090587
Camilovelag <camilo.vela.galindo@gmail.com>
2022-07-19 12:27:35 -0500

    Fix ESLint errors

--- scripts.js
+++ scripts.js
@@ -1,21 +1,21 @@
 function showMenu() {
-  let div = document.querySelector('#links-container');
+  const div = document.querySelector('#links-container');
   div.classList.toggle('active-menu');
 
-  let div2 = document.querySelector('#hamburger-btn');
+  const div2 = document.querySelector('#hamburger-btn');
   div2.classList.toggle('switch-btn');
 
-  let div3 = document.querySelector('#header-logo');
+  const div3 = document.querySelector('#header-logo');
   div3.classList.toggle('active-logo');
-  }
+}
 
 function hideMenu() {
-  let div4 = document.querySelector('#links-container');
+  const div4 = document.querySelector('#links-container');
   div4.classList.remove('active-menu');
 
-  let div5 = document.querySelector('#hamburger-btn');
+  const div5 = document.querySelector('#hamburger-btn');
   div5.classList.remove('switch-btn');
 
-  let div6 = document.querySelector('#header-logo');
+  const div6 = document.querySelector('#header-logo');
   div6.classList.remove('active-logo');
 }
