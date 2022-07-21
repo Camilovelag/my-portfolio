1fed192c64a39bf25bce6a4dc2e32cee1ce044e7
Camilovelag <camilo.vela.galindo@gmail.com>
2022-07-21 16:23:16 -0500

    Fix trailing comma

--- scripts.js
+++ scripts.js
@@ -130,7 +130,7 @@ if (window.matchMedia('(max-width: 768px)').matches) {
 const proj1 = {
   title: 'Multi Post Stories',
   img: './images/popup-img.png',
-  description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry´s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent'
+  description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry´s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
 };
 
 const btn1 = document.querySelector('.project-1');
