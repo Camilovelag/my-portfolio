accaede85d80579c2486624159cfa4605de2bb83
Camilovelag <camilo.vela.galindo@gmail.com>
2022-07-21 10:54:19 -0500

    Fix ESlint error

--- scripts.js
+++ scripts.js
@@ -50,52 +50,22 @@ menuContactUs.addEventListener('click', () => {
   div6.classList.remove('active-logo');
 });
 
-/* Open popup window section */
-
-// const proj1 = ['Multi Post Stories', './images/popup-img.png',
-//             'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry´s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
-//             '#',
-//             '#'];
-
-// const openPopupWindow = document.querySelector('.project-1');
-
-// openPopupWindow.addEventListener('click', () => {
-//   const div7 = document.querySelector('#popup');
-//   div7.classList.toggle('show-popup');
-//   document.getElementById('popup-title').innerHTML = proj1[0];
-//   document.getElementById('popup-img').src = proj1[1];
-//   document.getElementById('popup-description').innerHTML = proj1[2];
-//   document.getElementById("see-live > a").href = proj1[3];
-//   document.getElementById("see-source > a").href = proj1[4];
-
-// });
-
-// // Close popup
-// const closePopupWindow = document.querySelector('.popup-btn');
-
-// closePopupWindow.addEventListener('click', () => {
-//   const div8 = document.querySelector('#popup');
-//   div8.classList.toggle('show-popup');
-// });
-
-//Validation form--------------------------------------------------------------------------------------------
-
 const form = document.getElementById('form');
 const email = document.getElementById('email');
 const errorMessage = document.getElementById('error-msg');
 
-form.addEventListener("submit", (e) => {
+form.addEventListener('submit', (e) => {
   const emailValue = email.value;
   const upperCase = emailValue.replace(/[^A-Z]/g, '');
   if (upperCase.length > 0) {
-      e.preventDefault();
-      errorMessage.innerText = 'Please type your email in lowercase';
+    e.preventDefault();
+    errorMessage.innerText = 'Please type your email in lowercase';
   }
 });
 
 // form rearrange ----------------------
 const windowWidth = window.innerWidth;
-if (windowWidth > 768){
+if (windowWidth > 768) {
   document.getElementById('full_name').disabled = true;
 } else {
   document.getElementById('full_name').disabled = false;
