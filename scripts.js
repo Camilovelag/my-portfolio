dd8e6a6a9491d54bd588e8f2d34040090921efa3
Camilovelag <camilo.vela.galindo@gmail.com>
2022-07-22 18:57:46 -0500

    Fix popup info parsing error

--- scripts.js
+++ scripts.js
@@ -93,6 +93,11 @@ const arr1 = [
   },
 ];
 
+const lang1 = arr1[0].popuptec[0];
+const lang2 = arr1[0].popuptec[1];
+const lang3 = arr1[0].popuptec[2];
+const lang4 = arr1[0].popuptec[3];
+
 document.getElementById('card-works-container').innerHTML = arr1.map((items) => `<div class="card-works">  
               <h2 class="card-works-title" id="card-works-title">${items.popuptitle}</h2>
               <p class="card-works-description" id="card-works-description">${items.popupdescription}</p>
@@ -102,7 +107,7 @@ document.getElementById('card-works-container').innerHTML = arr1.map((items) => 
                 <li class="card-works-tech-items">${items.popuptec[2]}</li>
                 <li class="card-works-tech-items">${items.popuptec[3]}</li>
               </ul>
-                <a href="#popup" class="card-works-button project-2 project-${items.id}">See Project</a>
+                <a href="#popup" class="card-works-button project-2" id="project-${items.id}">See Project</a>
             </div>`).join('');
 
 // ---------------------------- OPEN POPUP WINDOW SECTION ----------------------------------
@@ -118,7 +123,7 @@ const tech1 = proj1.techList[0];
 const tech2 = proj1.techList[1];
 const tech3 = proj1.techList[2];
 
-const btn4 = document.querySelector('.project-4');
+const btn4 = document.querySelector('#project-4');
 const popupOpen = document.querySelector('.popup');
 
 btn4.addEventListener('click', () => {
@@ -133,25 +138,24 @@ btn4.addEventListener('click', () => {
   document.getElementById('popup-source').href = 'https://github.com/Camilovelag/MV_Portfolio';
 });
 
-const closePopupWindow = document.querySelector('.close-popup');
-closePopupWindow.addEventListener('click', () => {
-  popupOpen.classList.toggle('show-popup');
-});
-
-const btn0 = document.querySelector('.project-0');
+const btn0 = document.querySelector('#project-0');
 
 btn0.addEventListener('click', () => {
   popupOpen.classList.toggle('show-popup');
-  document.getElementById('popup-title').innerHTML = proj1.title;
-  document.getElementById('popup-techList1').innerHTML = tech1;
-  document.getElementById('popup-techList2').innerHTML = tech2;
-  document.getElementById('popup-techList3').innerHTML = tech3;
-  document.getElementById('popup-img').src = proj1.img;
-  document.getElementById('popup-description').innerHTML = proj1.description;
+  document.getElementById('popup-title').innerHTML = arr1[0].popuptitle;
+  document.getElementById('popup-techList1').innerHTML = lang1;
+  document.getElementById('popup-techList2').innerHTML = lang2;
+  document.getElementById('popup-techList3').innerHTML = lang3;
+  document.getElementById('popup-img').src = arr1[0].popupimg;
+  document.getElementById('popup-description').innerHTML = arr1[0].popupdescription;
   document.getElementById('popup-live').href = 'https://flunavgt.github.io/Federico-s_Portfolio/';
   document.getElementById('popup-source').href = 'https://github.com/Camilovelag/MV_Portfolio';
 });
 
+const closePopupWindow = document.querySelector('.close-popup');
+closePopupWindow.addEventListener('click', () => {
+  popupOpen.classList.toggle('show-popup');
+});
 
 
 
