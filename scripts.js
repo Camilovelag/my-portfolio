50743bc121b47485d9097f1687bc82d147aca5d5
Camilovelag <camilo.vela.galindo@gmail.com>
2023-03-28 13:06:39 -0500

    Update popup content

--- scripts.js
+++ scripts.js
@@ -63,9 +63,6 @@ const arr1 = [
     popupimg: './images/thumbnails/thumbnail-doctors-backend.jpg',
     btnlive: 'https://doctors-reservations-rily.onrender.com/api-docs/index.html',
     btnsource: 'https://github.com/Camilovelag/Doctors-Reservations-Backend',
-    btnName: 'btn1',
-    btnimg1: './pictures/TimeAndIcons/github.png',
-    btnimg2: './pictures/TimeAndIcons/github.png',
   },
   {
     id: 1,
@@ -75,9 +72,6 @@ const arr1 = [
     popupimg: './images/thumbnails/thumbnail-awesome-budget.jpg',
     btnlive: 'https://github.com/Camilovelag/my-awesome-budget-app',
     btnsource: 'https://my-awesome-budget-app.onrender.com/',
-    btnName: 'btn3',
-    btnimg1: './pictures/TimeAndIcons/github.png',
-    btnimg2: './pictures/TimeAndIcons/github.png',
   },
   {
     id: 2,
@@ -87,16 +81,8 @@ const arr1 = [
     popupimg: './images/thumbnails/thumbnail-awesome_recipes.jpg',
     btnlive: 'https://camilovelag-awesome-recipes.netlify.app',
     btnsource: 'https://github.com/Camilovelag/awesome-recipes',
-    btnName: 'btn4',
-    btnimg1: './pictures/TimeAndIcons/github.png',
-    btnimg2: './pictures/TimeAndIcons/github.png',
   },
 ];
-
-const lang1 = arr1[0].popuptec[0];
-const lang2 = arr1[0].popuptec[1];
-const lang3 = arr1[0].popuptec[2];
-const langStack1 = arr1[0].popuptec;
 
 document.getElementById('card-works-container').innerHTML = arr1.map((items) => `<div class="card-works" style="background-image: url('${items.popupimg}');">  
               <h2 class="card-works-title" id="card-works-title">${items.popuptitle}</h2>
@@ -105,7 +91,6 @@ document.getElementById('card-works-container').innerHTML = arr1.map((items) => 
                 <li class="card-works-tech-items">${items.popuptec[0]}</li>
                 <li class="card-works-tech-items">${items.popuptec[1]}</li>
                 <li class="card-works-tech-items">${items.popuptec[2]}</li>
-                <li class="card-works-tech-items">${items.popuptec[3]}</li>
               </ul>
                 <a href="#popup" class="card-works-button project-2" id="project-${items.id}">See Project</a>
             </div>`).join('');
@@ -113,29 +98,35 @@ document.getElementById('card-works-container').innerHTML = arr1.map((items) => 
 // ---------------------------- OPEN POPUP WINDOW SECTION ----------------------------------
 
 const proj1 = {
-  title: 'Multi Post Stories',
-  img: './images/popup-img.png',
-  description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry´s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
-  techList: ['Html', 'Bootstrap', 'Ruby on rails'],
+  popuptitle: 'Doctor Reservations Front-end',
+  popupdescription: 'Responsive and intuitive front-end app to manage doctors, logins, and reservations, which uses an API to manipulate and preserve data.',
+  popuptec: ['React', 'Redux', 'Tailwind'],
+  popupimg: './images/thumbnails/thumbnail-doctors-frontend.jpg',
+  btnlive: 'https://doctors-reservations.netlify.app/',
+  btnsource: 'https://github.com/Camilovelag/doctors-reservations-frontend',
+  btnName: 'btn4',
+  btnimg1: './pictures/TimeAndIcons/github.png',
+  btnimg2: './pictures/TimeAndIcons/github.png',
 };
 
-const tech1 = proj1.techList[0];
-const tech2 = proj1.techList[1];
-const tech3 = proj1.techList[2];
+
+const tech1 = proj1.popuptec[0];
+const tech2 = proj1.popuptec[1];
+const tech3 = proj1.popuptec[2];
 
 const btn4 = document.querySelector('#project-4');
 const popupOpen = document.querySelector('.popup');
 
 btn4.addEventListener('click', () => {
   popupOpen.classList.toggle('show-popup');
-  document.getElementById('popup-title').innerHTML = proj1.title;
+  document.getElementById('popup-title').innerHTML = proj1.popuptitle;
   document.getElementById('popup-techList1').innerHTML = tech1;
   document.getElementById('popup-techList2').innerHTML = tech2;
   document.getElementById('popup-techList3').innerHTML = tech3;
-  document.getElementById('popup-img').src = proj1.img;
-  document.getElementById('popup-description').innerHTML = proj1.description;
-  document.getElementById('popup-live').href = 'https://flunavgt.github.io/Federico-s_Portfolio/';
-  document.getElementById('popup-source').href = 'https://github.com/Camilovelag/MV_Portfolio';
+  document.getElementById('popup-img').src = proj1.popupimg;
+  document.getElementById('popup-description').innerHTML = proj1.popupdescription;
+  document.getElementById('popup-live').href = proj1.btnlive;
+  document.getElementById('popup-source').href = proj1.btnsource;
 });
 
 const btn0 = document.querySelector('#project-0');
@@ -157,13 +148,13 @@ const btn1 = document.querySelector('#project-1');
 btn1.addEventListener('click', () => {
   popupOpen.classList.toggle('show-popup');
   document.getElementById('popup-title').innerHTML = arr1[1].popuptitle;
-  document.getElementById('popup-techList1').innerHTML = lang1;
-  document.getElementById('popup-techList2').innerHTML = lang2;
-  document.getElementById('popup-techList3').innerHTML = lang3;
-  document.getElementById('popup-img').src = proj1.img;
+  document.getElementById('popup-techList1').innerHTML = arr1[1].popuptec[0];
+  document.getElementById('popup-techList2').innerHTML = arr1[1].popuptec[1];
+  document.getElementById('popup-techList3').innerHTML = arr1[1].popuptec[2];
+  document.getElementById('popup-img').src = arr1[1].popupimg;
   document.getElementById('popup-description').innerHTML = arr1[1].popupdescription;
-  document.getElementById('popup-live').href = 'https://flunavgt.github.io/Federico-s_Portfolio/';
-  document.getElementById('popup-source').href = 'https://github.com/Camilovelag/MV_Portfolio';
+  document.getElementById('popup-live').href = arr1[1].btnlive;
+  document.getElementById('popup-source').href = arr1[1].btnsource;
 });
 
 const btn2 = document.querySelector('#project-2');
@@ -171,13 +162,13 @@ const btn2 = document.querySelector('#project-2');
 btn2.addEventListener('click', () => {
   popupOpen.classList.toggle('show-popup');
   document.getElementById('popup-title').innerHTML = arr1[2].popuptitle;
-  document.getElementById('popup-techList1').innerHTML = lang1;
-  document.getElementById('popup-techList2').innerHTML = lang2;
-  document.getElementById('popup-techList3').innerHTML = lang3;
-  document.getElementById('popup-img').src = 'images/img_placeholder.png';
+  document.getElementById('popup-techList1').innerHTML = arr1[2].popuptec[0];
+  document.getElementById('popup-techList2').innerHTML = arr1[2].popuptec[1];
+  document.getElementById('popup-techList3').innerHTML = arr1[2].popuptec[2];
+  document.getElementById('popup-img').src = arr1[2].popupimg;
   document.getElementById('popup-description').innerHTML = arr1[2].popupdescription;
-  document.getElementById('popup-live').href = 'https://flunavgt.github.io/Federico-s_Portfolio/';
-  document.getElementById('popup-source').href = 'https://github.com/Camilovelag/MV_Portfolio';
+  document.getElementById('popup-live').href = arr1[2].btnlive;
+  document.getElementById('popup-source').href = arr1[2].btnsource;
 });
 
 const closePopupWindow = document.querySelector('.close-popup');
